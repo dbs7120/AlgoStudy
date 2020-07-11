08fca876e471a5a6347a7c6c2e9c19d0b0cac105
Adj <dbs7120@naver.com>
2020-07-11 17:12:08 +0900

    0711

---
+++ 1/1.cpp
@@ -0,0 +1,15 @@
+﻿//1. 1부터 N까지의 M의 배수합
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n, m, i, sum = 0;
+	cin >> n >> m;
+	for (i = 1; i <= n; i++)
+	{
+		if (i%m == 0)
+			sum += i;
+	}
+	cout << sum << endl;
+	return 0;
+}
---
+++ 2/2.cpp
@@ -0,0 +1,17 @@
+﻿//2. 자연수의 합
+// A부터 B까지의 합
+#include <iostream>
+using namespace std;
+int main()
+{
+	int A, B, i, sum = 0;;
+	cin >> A >> B;
+
+	for (i = A; i < B; i++)
+	{
+		cout << i << " + ";
+		sum += i;
+	}
+	cout << i << " = ";
+	cout << sum + i << endl;
+}
