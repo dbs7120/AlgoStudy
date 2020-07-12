0fb8850f1456d7e025d364945dc41423c383852f
Adj <dbs7120@naver.com>
2020-07-12 17:03:26 +0900

    0712

---
+++ 3/3.cpp
@@ -0,0 +1,20 @@
+﻿//3. 진약수의 합
+//수식출력시 규칙성파악
+#include <iostream>
+using namespace std;
+int main()
+{
+	int N, i, sum = 1;
+	cin >> N;
+	cout << "1";				// 어떤 수든 1은 반드시 포함 ( "1 + "고정 )
+	for (i = 2; i < N; i++)
+	{
+		if (N%i == 0)
+		{
+			cout << " + " << i;	// + a + b + c ..
+			sum += i;
+		}
+	}
+	cout << " = " << sum << endl;
+	return 0;
+}
---
+++ 4/4.cpp
@@ -0,0 +1,48 @@
+﻿//4. 나이차이
+//N명입력 최대 나이차이 출력
+#include <iostream>
+using namespace std;
+/*
+int main()							// my
+{
+	int N, i, j, temp, result = 0;
+	cin >> N;
+	int *input = new int[N];		// 동적할당
+	for (i = 0; i < N; i++)
+	{
+		cin >> input[i];
+	}
+
+	for (j = 0; j < N; j++)			// 정렬
+	{
+		for (i = 0; i < N - 1; i++)
+		{
+			if (input[i] > input[i + 1])
+			{
+				temp = input[i + 1];
+				input[i + 1] = input[i];
+				input[i] = temp;
+			}
+		}
+	}
+
+	result = input[N - 1] - input[0];	// 최대최소차이
+	cout << result << endl;
+	return 0;
+}
+*/
+
+int main()								// 강의방식
+{
+	freopen("input.txt", "rt", stdin);	// 파일입출력활용
+	int n, i, a, max = -2147000000, min = 2147000000;
+	cin >> n;
+	for (i = 1; i <= n; i++)
+	{
+		cin >> a;
+		if (a > max) max = a;
+		if (a < min) min = a;
+	}
+	cout << max - min << endl;
+	return 0;
+}
