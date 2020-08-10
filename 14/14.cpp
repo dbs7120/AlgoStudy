465602d62546c71a00bbc693ea212481b8f38b41
Adj <dbs7120@naver.com>
2020-08-10 22:46:49 +0900

    0810

---
+++ 14/14.cpp
@@ -0,0 +1,51 @@
+﻿// 14. 뒤집은 소수
+//
+
+#include <iostream>
+
+// 숫자뒤집기
+// x 3700	tmp = 0 res = 0
+// x 370	tmp = 0 res = 0*10 + 0
+// x 37		tmp = 7 res = 0*10 + 0*10 + 0
+// x 3		tmp = 3	res = 0*10 + 0*10 + 0*10 7*10 + 3
+
+int reverse(int x) {
+	int tmp, res = 0;
+	while (x > 0) {
+		tmp = x % 10;
+		res = res * 10 + tmp;
+		x = x / 10;
+	}
+	return res;
+}
+
+// 소수찾기
+bool isPrime(int x) {
+	int i;
+	bool flag = true;
+	if (x == 1)		// 1일경우 (뒤집기전 100 일때)
+		return false;
+	for (i = 2; i < x; i++) {
+		if (x % i == 0) {	// 자신까지 나눈 나머지가 존재할경우(약수존재) => 소수아님
+			flag = false;
+			break;
+		}
+	}
+	return flag;
+}
+
+int main()
+{
+	int n, num, i, tmp;
+	scanf_s("%d", &n);
+	for (i = 1; i <= n; i++) {
+		scanf_s("%d", &num);
+		tmp = reverse(num);
+		if (isPrime(tmp))
+			printf("%d ", tmp);
+
+	}
+
+
+	return 0;
+}
---
+++ 15/15.cpp
@@ -0,0 +1,38 @@
+﻿// 15. 소수의 개수(제한시간 1초)
+// 2<=N<=200000
+
+
+// 36 = 1 X 36
+//		2 X 18
+//		3 X 12
+//		4 X 9
+//		6 X 6
+//		2,3,4,6 만 확인한다면 가능
+//		j <= sqrt(36)
+//		<=> j*j <= 36
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int n, i, j, flag, cnt = 0;
+	scanf_s("%d", &n);
+	for (i = 2; i <= n; i++) {
+		flag = 1;
+		//for (j = 2; j*j <= i; j++) {
+		for (j = 2; j <= (int)sqrt(i); j++) {
+			if (i%j == 0) {
+				flag = 0;
+				break;
+			}
+		}
+		if (flag == 1)
+			cnt++;
+	}
+
+	printf("%d\n", cnt);
+
+}
+
+
