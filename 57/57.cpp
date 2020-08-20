c2cf3842a2172de0dc6b700fdb62c41142c2e63e
Adj <dbs7120@naver.com>
2020-08-21 00:35:13 +0900

    0820

---
+++ 57/57.cpp
@@ -0,0 +1,22 @@
+﻿// 57.재귀함수 이진수 출력
+//
+
+#include <stdio.h>
+
+void recur(int x) {
+	if (x == 0) return;
+	else {		
+		recur(x / 2); // 호출 및 출력 순서 주의
+		printf("%d", x % 2);
+	}
+}
+
+int main()
+{
+	int num;
+	scanf_s("%d", &num);
+	recur(num);
+	printf("\n");
+}
+
+
---
+++ 59/59.cpp
@@ -0,0 +1,32 @@
+﻿// 59.부분집합(DFS)
+// 깊이 (level) 방문 컨트롤
+// 이진트리 방식
+// 좌측이동 ch[L] = 1, 우측이동 ch[L] = 0
+// 결과 ch[] 값이 1인 배열의 인덱스출력
+// 총 부분집합 갯수 : 2^n
+
+#include <stdio.h>
+int n, ch[11];
+void DFS(int L) {
+	if (L == n + 1) {
+		for (int i = 1; i <= n; i++) {
+			if (ch[i] == 1)
+				printf("%d ", i);
+		}
+		printf("\n");
+		return;
+	}
+	else {
+		ch[L] = 1;
+		DFS(L + 1);
+		ch[L] = 0;
+		DFS(L + 1);
+	}
+}
+
+int main()
+{
+	scanf_s("%d", &n);
+	DFS(1);
+}
+
